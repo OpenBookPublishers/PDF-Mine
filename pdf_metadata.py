da4832ded1981ec5787579cc8224dc47401c8a73
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2015-04-14 01:54:35 +0100

    added script to extract link metadata;

---
+++ pdf_metadata.py
@@ -0,0 +1,13 @@
+# Print link metadata of a PDF file, in JSON format
+# Nik Sultana, Open Book Publishers, April 2015
+#
+# Usage:
+#   python pdf_metadata.py ../StoriesQuechanOralLiterature.pdf > sqol-linkmetadata.json
+
+import pdfmine
+import sys
+
+filename = sys.argv
+pdf = pdfmine.PDFMine(filename[1])
+print pdf.metadata_to_json()
+pdf.close()
