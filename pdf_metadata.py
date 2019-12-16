b35b2890ee31a4427d0357b85ddf08cabc3c3362
Javier Arias <javier@arias.re>
2019-12-16 12:09:39 +0000

    tidy secondary scripts

--- pdf_metadata.py
+++ pdf_metadata.py
@@ -2,12 +2,13 @@
 # Nik Sultana, Open Book Publishers, April 2015
 #
 # Usage:
-#   python pdf_metadata.py ../StoriesQuechanOralLiterature.pdf > sqol-linkmetadata.json
+#   python pdf_metadata.py ../StoriesQuechanOralLiterature.pdf > \
+#        sqol-linkmetadata.json
 
 import pdfmine
 import sys
 
 filename = sys.argv
 pdf = pdfmine.PDFMine(filename[1])
-print pdf.metadata_to_json()
+print(pdf.metadata_to_json())
 pdf.close()
--- testpdfmine.py
+++ testpdfmine.py
@@ -1,8 +1,8 @@
 import pdfmine
 import sys
 
-filename=sys.argv
-pdf=pdfmine.PDFMine(filename[1])
+filename = sys.argv
+pdf = pdfmine.PDFMine(filename[1])
 # pdf.save_video(filename[2]) FIXME untested feature
 pdf.test()
 pdf.close()
